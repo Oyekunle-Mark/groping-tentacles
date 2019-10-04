d6b7e490e758ef9e4aa65b1b6609adce13b09c9d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-04 15:55:51 +0100

    Implementation of the quick sort algorithm

---
+++ quick_sort.py
@@ -0,0 +1,13 @@
+def quick_sort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+        less_than_pivot = [num for num in arr[1:] if num < pivot]
+        greater_than_pivot = [num for num in arr[1:] if num > pivot]
+
+        return quick_sort(less_than_pivot) + [pivot] + quick_sort(greater_than_pivot)
+
+
+print(quick_sort([5, 3, 6, 2, 10]))
+print(quick_sort([12, 3, 232, 32, -3, 0, 23, -2, 2]))
