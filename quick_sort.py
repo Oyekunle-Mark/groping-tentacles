1581971e5923a18bfb9552db8b4dc18d69bbcffc
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-04 16:00:08 +0100

    Added a docstring

--- quick_sort.py
+++ quick_sort.py
@@ -1,4 +1,12 @@
 def quick_sort(arr):
+    """Implementation of the quick sort algorithm
+
+    Arguments:
+        arr {list} -- array to be sorted
+
+    Returns:
+        list -- the sorted list
+    """
     if len(arr) < 2:
         return arr
     else:
