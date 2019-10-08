d26ec1582c35e01e68b904c3cd721e5a4bf91e79
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-08 17:13:06 +0100

    Represents a graph

---
+++ graph.py
@@ -0,0 +1,14 @@
+from pprint import pprint as pp
+
+# use a hash table to hold the graph
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
+
+pp(graph)
