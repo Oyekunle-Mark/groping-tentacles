213e4d9f2e33db32bd4394ac52a0e24130130edc
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-08 17:32:40 +0100

    Checks for jonny in the network

--- graph.py
+++ graph.py
@@ -12,7 +12,36 @@ graph["peggy"] = []
 graph["thom"] = []
 graph["jonny"] = []
 
+print("The graph")
 pp(graph)
 
-search_queue = deque()
-search_queue += graph["you"]
+
+def is_jonny(person):
+    return person == "jonny"
+
+
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+
+    # to keep track of who has been searched
+    searched = []
+
+    while search_queue:
+        current_person = search_queue.popleft()
+
+        # if person has not been searched
+        if not current_person in searched:
+            if is_jonny(current_person):
+                print(f"{current_person} found!")
+                return True
+            else:
+                search_queue += graph[current_person]
+
+                # mark current_person as searched
+                searched.append(current_person)
+
+    return False
+
+
+print(search("you"))
